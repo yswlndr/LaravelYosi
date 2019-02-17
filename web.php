c20981e22dc3711092777b818447ee76713d878e
yswlndr <46546161+yswlndr@users.noreply.github.com>
2019-02-17 19:53:09 +0700

    tokap 7

---
+++ web.php
@@ -0,0 +1,39 @@
+  <?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome'); 
+}); 
+
+Route:: group(['middleware'=>['auth']], function(){
+	Route::prefix('admin')->group(function(){
+	Route::get('/', function(){
+		return view('admin.pages.dashboard');
+	})->name('admin.home');
+
+	Route::prefix('user')->group(function(){
+		
+		Route::get('/','UserController@daftar')->name('admin.user')->middleware('akses.admin');
+
+		Route::get('/add','UserController@add')->name('admin.user.add')->middleware('akses.admin');
+		Route::post('/add','UserController@save')->middleware('akses.admin');
+
+		Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
+		Route::post('/setting','UserSettingController@update');
+	});
+});
+});
+
+Auth::routes();
+
+
